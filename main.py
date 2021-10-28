414c164b74dc0bb5139aa49e4886d3ffb46b74cf
Elly Wong <elly.yywong@gmail.com>
2021-10-28 10:26:51 -0700

    added

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 def my_function(fname):
   print(fname + " Goslee")
 
-my_function("Trenisha")
+my_function("Trenisha1")
 
 
 
-def simple_name_concat(first, last):
-  return first + " " + last
+def simple_name_concat(first, last1):
+  return first + " " + last1
   
